cf281dd128db108b4efa73c911a9da97ee53ce22
alexmochu <mochualex4@gmail.com>
2021-10-02 09:31:28 +0300

    setup routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,77 @@
 // client/src/App.js
 
-import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
-function App() {
-  const [data, setData] = React.useState(null);
+import styled from 'styled-components';
 
-  React.useEffect(() => {
+
+const AppDiv = styled.div`
+  text-align: center;
+`;
+
+const AppHeader = styled.header`
+  background-color: #282c34;
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  font-size: calc(10px + 2vmin);
+  color: white;
+`;
+
+export default function App() {
+  return (
+    <Router>
+      <div>
+        <section>
+          <ul>
+              <li>
+                <Link to="/">Dashoard</Link>
+              </li>
+              <li>
+                <Link to="/people-detail">People Detail</Link>
+              </li>
+            </ul>
+        </section>
+        <Switch>
+          <Route path="/people-detail">
+            <PeopleDetail />
+          </Route>
+          <Route path="/">
+            <Dashboard />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
+}
+
+function Dashboard() {
+    const [data, setData] = React.useState(null);
+
+    React.useEffect(() => {
     fetch("/api")
       .then((res) => res.json())
       .then((data) => setData(data.message));
   }, []);
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <AppDiv>
+      <AppHeader>
         <p>{!data ? "Loading..." : data}</p>
-      </header>
-    </div>
+      </AppHeader>
+    </AppDiv>
   );
 }
 
-export default App;
+function PeopleDetail() {
+  return <h2>PeopleDetail</h2>;
+}
+
